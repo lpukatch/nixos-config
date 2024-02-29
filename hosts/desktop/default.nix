69047e9b8fa2217920bd83bcec7ec32d241148ef
lpukatch <lpukatch@gmail.com>
2024-02-29 10:32:28 -0500

    lol enable experimental on desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -136,7 +136,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
